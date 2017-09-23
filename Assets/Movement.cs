1a3342b9fbc0522761fb5afcbdeb64e7cd60accf
ay-y <31833973+ay-y@users.noreply.github.com>
2017-09-23 15:49:50 -0400

    fix movement

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,37 +7,49 @@ public class Movement : MonoBehaviour {
 
     private Rigidbody rigidBody;
     private bool onGround = false;
+    private bool onLeft = false;
+    private bool onRight = false;
+
 	private SpriteRenderer sprRend;
-    public float velocityMultiplier;
+    public float speedMultiplier;
     public float jumpForce;
-	private Animator anim;
+    public float maxSpeed;
+    public float groundFriction;
+    public float airFriction;
+
+    private Animator anim;
 
     // Use this for initialization
     void Start () {
         rigidBody = GetComponent<Rigidbody>();
-		sprRend = GetComponent<SpriteRenderer>();
-		anim = GetComponent<Animator> ();
+        sprRend = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
 
-
-	}
+    }
 
     private void OnTriggerEnter(Collider other)
     {
-        onGround = true;
+       // if (other.name == "player")
+            onGround = true;
+
     }
 
     private void OnTriggerExit(Collider other)
     {
-        onGround = false;
+       // if (other.name == "player")
+            onGround = false;
     }
 
 
     // Update is called once per frame
     void Update () {
 
-        rigidBody.velocity = new Vector3(Input.GetAxis("Horizontal") * velocityMultiplier, rigidBody.velocity.y, rigidBody.velocity.z);
-		anim.SetFloat ("speed", rigidBody.velocity.x);
-		if (rigidBody.velocity.x < 0) {
+        //if (onGround)
+        rigidBody.AddForce(new Vector3(Input.GetAxis("Horizontal") * speedMultiplier, 0.0f, 0.0f));
+
+
+        anim.SetFloat("speed", rigidBody.velocity.x);
+        if (rigidBody.velocity.x < 0) {
 			sprRend.flipX = true;
 		}
 		if (rigidBody.velocity.x > 0) {
@@ -44,6 +57,29 @@ public class Movement : MonoBehaviour {
 		}
         if (onGround && Input.GetKeyDown("space"))
             rigidBody.AddForce(Vector3.up * jumpForce);
+
+
+
+        if (onGround)
+            rigidBody.velocity = new Vector3(rigidBody.velocity.x * groundFriction, rigidBody.velocity.y, rigidBody.velocity.z);
+        else
+            rigidBody.velocity = new Vector3(rigidBody.velocity.x * airFriction, rigidBody.velocity.y, rigidBody.velocity.z);
+
+    }
+    void FixedUpdate()
+    {
+        Vector3 left = transform.TransformDirection(Vector3.left);
+        Vector3 right = transform.TransformDirection(Vector3.right);
+
+
+        if (rigidBody.velocity.x > maxSpeed)
+        {
+            rigidBody.velocity = new Vector3(maxSpeed, rigidBody.velocity.y, 0.0f);
+        }
+        if (rigidBody.velocity.x < -maxSpeed)
+        {
+            rigidBody.velocity = new Vector3(-maxSpeed, rigidBody.velocity.y, 0.0f);
+        }
 
     }
 }
