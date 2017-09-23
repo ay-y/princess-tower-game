0a26c9a60a1589c06fbfa42411eae879ed6e817b
Arlunn <32222430+Arlunn@users.noreply.github.com>
2017-09-23 19:12:31 -0400

    prince

---
+++ Assets/LevelChange.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelChange : MonoBehaviour {
+
+    private Rigidbody rigidBody;
+	// Use this for initialization
+	void Start () {
+        rigidBody = GetComponent<Rigidbody>();
+	}
+    private void OnTriggerEnter(Collider other)
+    {
+
+        Application.LoadLevel("Tower2");
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
