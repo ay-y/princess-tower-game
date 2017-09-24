23c3baf220a3859809d4a087c0f37252eec04e3c
ay-y <31833973+ay-y@users.noreply.github.com>
2017-09-24 17:32:23 -0400

    tower3 and stuff

---
+++ Assets/BGMScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGMScript : MonoBehaviour
+{
+
+    //AudioSource audioSource;
+    public GameObject ay;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        //audioSource = GetComponent<AudioSource>();
+        ay.GetComponent<Player>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (ay.GetComponent<Player>().notplayed == true)
+        {
+            //audioSouce.mute;
+        }
+    }
+}
